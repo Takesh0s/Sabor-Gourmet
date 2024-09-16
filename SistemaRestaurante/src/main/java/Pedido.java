c56d4e63575f92e1b250a16a1f84f7c1f42af8df
Luiz Phillipe <107925073+Takesh0s@users.noreply.github.com>
2024-09-15 23:39:18 -0300

    Update Pedido.java

--- SistemaRestaurante/src/main/java/Pedido.java
+++ SistemaRestaurante/src/main/java/Pedido.java
@@ -1,27 +1,27 @@
 package SistemaRestaurante;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Pedido {
     private Cliente cliente;
     private ArrayList<ItemMenu> itens;
     private double total;
     private String status;
+    private Timer timer;
 
     public Pedido(Cliente cliente) {
         this.cliente = cliente;
         this.itens = new ArrayList<>();
         this.total = 0.0;
-        this.status = "Em preparação";
+        this.status = "Em preparacao";
+        iniciarTimer();
     }
 
     public void adicionarItem(ItemMenu item) {
         itens.add(item);
         total += item.calcularPreco();
-    }
-
-    public double calcularTotal() {
-        return total;
     }
 
     public void atualizarStatus(String novoStatus) {
@@ -33,11 +33,27 @@ public class Pedido {
         for (ItemMenu item : itens) {
             System.out.println(item.getDetalhesItem());
         }
-        System.out.println("Total: R$" + total);
+        System.out.println("Total: R$" + String.format("%.2f", total));
         System.out.println("Status do pedido: " + status);
+        System.out.println();
     }
 
-    public String getStatus() {
-        return status;
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    private void iniciarTimer() {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (status.equals("Em preparacao")) {
+                    atualizarStatus("Saiu para entrega");
+                } else if (status.equals("Saiu para entrega")) {
+                    atualizarStatus("Entregue");
+                    timer.cancel();
+                }
+            }
+        }, 40000, 40000);
     }
 }
