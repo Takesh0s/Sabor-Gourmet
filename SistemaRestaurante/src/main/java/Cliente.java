19240180401f44b0673d7697207a97cbf28c8339
Luiz Phillipe <107925073+Takesh0s@users.noreply.github.com>
2024-09-15 23:38:27 -0300

    Update Cliente.java

--- SistemaRestaurante/src/main/java/Cliente.java
+++ SistemaRestaurante/src/main/java/Cliente.java
@@ -4,24 +4,25 @@ public class Cliente {
     private String nome;
     private String endereco;
     private String telefone;
-    
-    public Cliente(String nome, String endereco, String telefone){
+
+    public Cliente(String nome, String endereco, String telefone) {
         this.nome = nome;
         this.endereco = endereco;
         this.telefone = telefone;
     }
-    
-    public void visualizarCliente(){
-    System.out.println("Nome: " + this.nome);
-    System.out.println("Endereco: " + this.endereco);
-    System.out.println("Telefone: " + this.telefone);
+
+    public void visualizarCliente() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Endereco: " + this.endereco);
+        System.out.println("Telefone: " + this.telefone);
+        System.out.println();
     }
-    
-    public static Cliente registrarCliente(String nome, String endereco, String telefone){
+
+    public static Cliente registrarCliente(String nome, String endereco, String telefone) {
         return new Cliente(nome, endereco, telefone);
     }
-    
-    public String getNome(){
-    return nome;
+
+    public String getNome() {
+        return nome;
     }
 }
